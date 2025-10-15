13dd03f0532aa43bc42ae29b82f1142fd1fb76ff
quaternion-fumbler <quaternion.fumbler@gmail.com>
2025-10-15 17:13:44 -0400

    Test commit

---
+++ sketch_hawkeye/sketch_hawkeye.ino
@@ -0,0 +1,38 @@
+
+// Will just be ESP32
+// to do:
+/*
+
+
+*/
+
+// Variables
+int rocket_state = 0; // 0 is on pad, 1 is in flight
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  /*
+  switch
+    case 0:
+      if (accel magnitude != gravity){
+        rocket_state = 1
+      }
+    case 1:
+      if (accel magnitude == gravity && (t1 <= time_since_launch <= t2)){
+        trigger camera and parachute pyrocharge
+      }
+  
+  */
+  tempMethod();
+
+}
+
+void tempMethod(){
+  printf("Hello World!");
+}
