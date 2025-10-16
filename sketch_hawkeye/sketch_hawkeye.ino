eb40c5150b17b557db5a44d9ae46a19786d24def
quaternion-fumbler <quaternion.fumbler@gmail.com>
2025-10-15 20:17:57 -0400

    Initial changes in transforming pseudocode into starting code. Necessary variables were added, along with making the temporary function have a purpose.

--- sketch_hawkeye/sketch_hawkeye.ino
+++ sketch_hawkeye/sketch_hawkeye.ino
@@ -8,6 +8,12 @@
 
 // Variables
 int rocket_state = 0; // 0 is on pad, 1 is in flight
+double gravity = 9.8;
+double gravity_margin = 0.0; // same thing as time margin, but for gravity
+double accel_x = 0.0, accel_y = 0.0, accel_z = 0.0; // acceleration values
+long time_margin = 0.0; // just a fudge/margin factor, as unlikely the esp32 will directly measure the exact time of apogee
+long time_since_launch = 0.0; // self explanatory
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,22 +23,23 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   
-  /*
+  
   switch
     case 0:
-      if (accel magnitude != gravity){
+      if (magnitude_accel() != gravity){
         rocket_state = 1
       }
     case 1:
-      if (accel magnitude == gravity && (t1 <= time_since_launch <= t2)){
+      
+      if ((magnitude_accel()) == gravity && (t1 <= time_since_launch <= t2)){
         trigger camera and parachute pyrocharge
       }
   
   */
-  tempMethod();
+  magnitude_accel();
 
 }
 
-void tempMethod(){
-  printf("Hello World!");
+double magnitude_accel(){
+  return 0.0;
 }
